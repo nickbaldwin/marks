4dc6c3996c17b0cd05ec453fa5436d4abd1c691b
Nick Baldwin <nick.with.a.z@gmail.com>
2024-05-17 14:53:01 +1000

    react 18 render

--- src/content-script.tsx
+++ src/content-script.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { storeReadyPromise } from './store/store';
 import { Content } from './newtab/Content';
 
 if (storeReadyPromise !== null) {
     storeReadyPromise.then(() => {
-        const root: HTMLElement | null =
+        const container: HTMLElement | null =
             document.getElementById('content-root');
-        if (root) {
-            ReactDOM.createRoot(root).render(
+        if (container) {
+            createRoot(container).render(
                 <React.StrictMode>
                     <Content />
                 </React.StrictMode>
