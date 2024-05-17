9e7eb43650fe65e508f025c05449c877ffb3ee06
Nick Baldwin <nick.with.a.z@gmail.com>
2024-05-17 14:46:48 +1000

    test state actions

---
+++ src/store/storeActionFunctions.test.ts
@@ -0,0 +1,57 @@
+import { BasicInfo, Folder, FoldersMap } from './schema';
+
+// this will be used in early dev process to check functions, then skipped and
+// behaviour will be checked by rendering components directly
+
+describe('increase', () => {
+    it('increase fn works', () => {
+        let state = { bears: 0 };
+        const inc = (by: number): void => {
+            const fn = (state: { bears: number }) => ({
+                bears: state.bears + by,
+            });
+            state = fn(state);
+        };
+        inc(1);
+        expect(state).toEqual({ bears: 1 });
+        inc(2);
+        expect(state).toEqual({ bears: 3 });
+    });
+});
+
+describe('add folder', () => {
+    it('adds a folder', () => {
+        let state = { foldersList: ['1'], foldersMap: {} };
+        const addFolder = (folder: BasicInfo): void => {
+            const f = new Folder(folder);
+            const fn = (state: {
+                foldersList: string[];
+                foldersMap: FoldersMap;
+            }) => ({
+                foldersList: [...state.foldersList, f.id],
+                foldersMap: {
+                    ...state.foldersMap,
+                    [f.id]: folder,
+                },
+            });
+            state = fn(state);
+        };
+        addFolder({
+            title: 'folder',
+            description: 'folder',
+        });
+        expect(state.foldersList.length).toEqual(2);
+        const id = state.foldersList[1];
+        expect(state).toEqual({
+            foldersList: ['1', id],
+            foldersMap: {
+                [id]: {
+                    // todo!
+                    // id: id,
+                    title: 'folder',
+                    description: 'folder',
+                },
+            },
+        });
+    });
+});
